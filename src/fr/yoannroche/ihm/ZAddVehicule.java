d9404e1a4d517d6efc56c3eb4550bd427fd5bdba
yoann roche <el-rambo-poto@hotmail.fr>
2018-08-03 18:54:35 +0200

    Modif des ajouts options dans l'AddVehicule

--- src/fr/yoannroche/ihm/ZAddVehicule.java
+++ src/fr/yoannroche/ihm/ZAddVehicule.java
@@ -49,7 +49,7 @@ public class ZAddVehicule extends JDialog {
 	private List<Option> optionV = new ArrayList<Option>();
 	private int moteurID = 0;
 	private int marqueID = 0;
-	
+
 
 
 
@@ -67,7 +67,6 @@ public class ZAddVehicule extends JDialog {
 
 		try {
 
-
 			JPanel panVehicule = new JPanel();
 			panVehicule.setBackground(Color.white);
 			panVehicule.setPreferredSize(new Dimension(200,55));
@@ -77,40 +76,35 @@ public class ZAddVehicule extends JDialog {
 			nom.setBackground(Color.white);
 			nom.setPreferredSize(new Dimension(150,40));
 			panVehicule.add(nom);
-
 			JPanel panMarque = new JPanel();
 			panMarque.setBackground(Color.white);
 			panMarque.setPreferredSize(new Dimension(200,55));
 			panMarque.setBorder(BorderFactory.createLineBorder(Color.black));
-
 			final DAO<Marque> marqueDao = DAOFactory.getMarqueDAO();
 			marque = new JComboBox<String>();
-			for(int i=0;i<4;i++) {
+			for(int i=0;i<3;i++) {
 				marque.addItem(marqueDao.find(i).getNom());
 			}
-			
+
 			marque.setBorder(BorderFactory.createTitledBorder(" Marque du Vehicule "));
 			marque.setBackground(Color.white);
 			marque.setPreferredSize(new Dimension(170,40));
 			panMarque.add(marque);
-
 			JPanel panMoteur = new JPanel();
 			panMoteur.setBackground(Color.white);
 			panMoteur.setPreferredSize(new Dimension(200,55));
 			panMoteur.setBorder(BorderFactory.createLineBorder(Color.black));
-
 			final DAO<Moteur> moteurDao = DAOFactory.getMoteurDAO();
 			moteurL = new JComboBox<String>();
-			
+
 			for(int i =0;i<9;i++) {
 				moteurL.addItem(moteurDao.find(i).getCylindre());
 			}
-			
+
 			moteurL.setBorder(BorderFactory.createTitledBorder(" Marque du Vehicule "));
 			moteurL.setBackground(Color.white);
 			moteurL.setPreferredSize(new Dimension(170,40));
 			panMoteur.add(moteurL);
-
 			JPanel panPrix = new JPanel();
 			panPrix.setBackground(Color.white);
 			panPrix.setPreferredSize(new Dimension(280,55));
@@ -120,31 +114,23 @@ public class ZAddVehicule extends JDialog {
 			prix.setBackground(Color.white);
 			prix.setPreferredSize(new Dimension(250,40));
 			panPrix.add(prix);
-
 			final DAO<Option> optionDao = DAOFactory.getOptionDAO();
 			JPanel panOption = new JPanel();
 			JPanel option = new JPanel();
-		
-			
-			
-			for(int i =0;i<5;i++) {
-				 optionL = new JCheckBox(optionDao.find(i).getNom());
-				option.add(optionL);
-				
-			}
-			
-			
-
-			
+			final JCheckBox option1 = new JCheckBox(optionDao.find(0).getNom());
+			final JCheckBox option2 = new JCheckBox(optionDao.find(1).getNom());
+			final JCheckBox option3 = new JCheckBox(optionDao.find(2).getNom());
+			final JCheckBox option4 = new JCheckBox(optionDao.find(3).getNom());
+			final JCheckBox option5 = new JCheckBox(optionDao.find(4).getNom());
+			option.add(option1);
+			option.add(option2);
+			option.add(option3);
+			option.add(option4);
+			option.add(option5);		
 			panOption.setBackground(Color.white);
 			panOption.setPreferredSize(new Dimension(550,75));
-			panOption.setBorder(BorderFactory.createLineBorder(Color.black));
-			
-			option.setPreferredSize(new Dimension(520,60));
-
-		    
-
-		    
+			panOption.setBorder(BorderFactory.createLineBorder(Color.black));		
+			option.setPreferredSize(new Dimension(520,60));	    
 			option.setBorder(BorderFactory.createTitledBorder(" Options Disponibles "));
 			cancel.setPreferredSize(new Dimension(100,30));
 			cancel.setBorder(BorderFactory.createLineBorder(Color.black));
@@ -154,7 +140,6 @@ public class ZAddVehicule extends JDialog {
 				public void actionPerformed(ActionEvent arg0) {
 					setVisible(false);	
 				}
-
 			});
 			ok.setPreferredSize(new Dimension(100,30));
 			ok.setBorder(BorderFactory.createLineBorder(Color.black));
@@ -163,8 +148,6 @@ public class ZAddVehicule extends JDialog {
 				public void actionPerformed(ActionEvent arg0) {  
 
 					try {
-						
-						
 
 						moteurID = moteurL.getSelectedIndex();
 						marqueID = marque.getSelectedIndex();
@@ -175,13 +158,26 @@ public class ZAddVehicule extends JDialog {
 						vehicule.setPrix(price);
 						vehicule.setNom(nom.getText());
 						vehicule.setId(vehicule.getId());
-						if(optionL.isSelected()==true) {
-							System.out.println("test");
+						if(option1.isSelected()==true) {
+							vehicule.addOption(optionDao.find(0));
 						}
+						if(option2.isSelected()==true) {
+							vehicule.addOption(optionDao.find(1));
+						}
+						if(option3.isSelected()==true) {
+							vehicule.addOption(optionDao.find(2));
+						}
+						if(option4.isSelected()==true) {
+							vehicule.addOption(optionDao.find(3));
+						}
+						if(option5.isSelected()==true) {
+							vehicule.addOption(optionDao.find(4));
+						}
+
 						vehicule = vehiculeDao.create(vehicule);
-				
-						 obs.updateObservateur();   //Lancement de la méthode update de l'Observateur.
-						 
+
+						obs.updateObservateur();   //Lancement de la méthode update de l'Observateur.
+
 						setVisible(false);
 
 						logger.info("Option(s) ajoutée(s) au véhicule : " + optionV);
@@ -190,17 +186,11 @@ public class ZAddVehicule extends JDialog {
 						arg1.printStackTrace();
 						JOptionPane.showMessageDialog(null, "Vous devez entrer le nom du vehicule et son prix !", "Erreur", JOptionPane.ERROR_MESSAGE);
 					}
-
-
 				}   
-
 			});
 
 			option.setBackground(Color.white);
 			panOption.add(option);
-
-
-
 			JPanel content = new JPanel();
 			content.add(panVehicule);
 			content.add(panMarque);
@@ -220,18 +210,12 @@ public class ZAddVehicule extends JDialog {
 			content.add(ok);
 			content.add(cancel);
 			content.setBackground(Color.getHSBColor(0.550f, 0.40f, 0.90f));
-
 			this.getContentPane().add(content,BorderLayout.CENTER);
 			this.setVisible(true);
-
 		} catch (Exception e) {
 			e.printStackTrace();
-
 		} 
-
-
 	}
-
 }
 
 
